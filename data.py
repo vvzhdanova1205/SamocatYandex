1b2ef547f1df7b259e461a7e6af6d0809d2a7ea2
vvzhdanova1205 <vvzhdanova1205@yandex.ru>
2023-10-09 00:13:45 +0200

    My diploma

---
+++ data.py
@@ -0,0 +1,17 @@
+headers = {
+    "Content-Type": "application/json"
+}
+
+order_body = {
+    "firstName": "Garry",
+    "lastName": "Potter",
+    "address": "Privet Drive 4",
+    "metroStation": 4,
+    "phone": "+79991112223",
+    "rentTime": 5,
+    "deliveryDate": "2020-10-10",
+    "comment": "Not Slytherin!",
+    "color": [
+        "BLACK"
+    ]
+}
